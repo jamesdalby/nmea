89a58eebd625b840761b81c4f50ff12773ed4cc5
james <jdalby@gmail.com>
2022-03-07 15:33:56 +0000

    dart warning cleanup on test main

--- test/nmea_test.dart
+++ test/nmea_test.dart
@@ -1,5 +1,5 @@
 import 'package:nmea/nmea.dart';
 
 void main(final List<String> args) {
-  NMEASocketReader nmea = new NMEASocketReader(args[0], int.parse(args[1]), print);
+  /* NMEASocketReader nmea = */ new NMEASocketReader(args[0], int.parse(args[1]), print);
 }
